70f7e06b3533036cfd4124d8915d3f87c90a9afc
Jingsi Zhu <zhu@jing.si>
2014-01-01 12:13:52 +0800

    work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+function turnToSection(sec) {
+	if ($('body').hasClass("home")) {
+		var home = $('section.home');
+		home.css('margin-top', -home.outerHeight());
+		var callback = function () {
+			$('body').removeClass('home');
+		}
+		home.one('webkitTransitionEnd', callback);
+		home.one('transitionEnd', callback);
+	}
+}
 
+
+$(function(){
+	$(".home .description").each(function(id, desNode){
+		$(desNode).find(".current a").click(function(){
+			var prevNode = $(desNode).find(".previous");
+			if (prevNode.hasClass("collapsed")) {
+				prevNode.removeClass("collapsed").css('max-height', prevNode.find(".inner").height());
+			} else {
+				prevNode.addClass("collapsed").css('max-height', '');
+			}
+		})
+	});
+});
