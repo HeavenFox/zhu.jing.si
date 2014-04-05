6bd099d304b7f906b70dc91eaf2a66c933158386
Jingsi Zhu <zhu@jing.si>
2014-04-05 16:54:00 -0700

    Make script for minifying js

--- _build/make.sh
+++ _build/make.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
 sass --update $BASEDIR/../_sass:$BASEDIR/../css -t compressed
+curl --data-urlencode js_code@$BASEDIR/../_js/main.js -d output_format=text -d output_info=compiled_code http://closure-compiler.appspot.com/compile > $BASEDIR/../js/main.js
